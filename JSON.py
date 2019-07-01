67f7fa8be4e497ed9e85335d2ee8a0f5c577cb4c
kanth0447 <sreekanthtagirise@gmail.com>
2019-07-01 07:46:51 +0530

    message

---
+++ JSON.py
@@ -0,0 +1,34 @@
+import subprocess
+import shlex
+import json
+
+def main():
+    df_array = [shlex.split(x) for x in
+                subprocess.check_output(["df","-h"]).rstrip().split('\n')]
+    df_num_lines = df_array[:].__len__()
+    df_json = {}
+    df_json["filesystems"] = []
+    for row in range(1, df_num_lines):
+        df_json["filesystems"].append(df_to_json(df_array[row]))
+        print json.dumps(df_json, sort_keys=True, indent=2)
+        return
+
+def df_to_json(tokenList):
+    result = {}
+    fsName = tokenList[0]
+    fsSize = tokenList[1]
+    fsUsed = tokenList[2]
+    fsAvail = tokenList[3]
+    fsUse = tokenList[4]
+    fsMountPoint = tokenList[5]
+    result["filesystem"] = {}
+    result["filesystem"]["name"] = fsName
+    result["filesystem"]["size"] = fsSize
+    result["filesystem"]["used"] = fsUsed
+    result["filesystem"]["Avail"] = fsAvail
+    result["filesystem"]["use%"] = fsUse
+    result["filesystem"]["mount_point"] = fsMountPoint
+    return result
+
+if __name__ == '__main__':
+    main()
--- sample.py
+++ sample.py
@@ -1,6 +1,7 @@
 my_dict = {}
+my_dict1 = {}
 index = 0
-my_list = []
+#my_list = []
 my_list1 = []
 my_list2 = []
 my_list3 = []
@@ -8,26 +9,34 @@ my_list4 = []
 my_list5 = []
 with open('output.txt') as fileobj:
     for line in fileobj:
+        my_list = []
         global index
         if index == 0:
             key = line.strip().split(None)
+            my_dict[key[0]] = []
+            my_dict[key[1]] = []
+            my_dict[key[2]] = []
+            my_dict[key[3]] = []
+            my_dict[key[4]] = []
+            my_dict[key[5]] = []
         else:
             value = line.strip().split(None)
             my_list.append(value[0])
-            my_list1.append(value[1])
-            my_list2.append(value[2])
-            my_list3.append(value[3])
-            my_list4.append(value[4])
-            my_list5.append(value[5])
-            my_dict[key[0]] = my_list
-            my_dict[key[1]] = my_list1
-            my_dict[key[2]] = my_list2
-            my_dict[key[3]] = my_list3
-            my_dict[key[4]] = my_list4
-            my_dict[key[5]] = my_list5
+            my_list.append(value[1])
+            my_list.append(value[2])
+            my_list.append(value[3])
+            my_list.append(value[4])
+            my_list.append(value[5])
+            my_dict[key[0]].append(my_list[0])
+            my_dict[key[1]].append(my_list[1])
+            my_dict[key[2]].append(my_list[2])
+            my_dict[key[3]].append(my_list[3])
+            my_dict[key[4]].append(my_list[4])
+            my_dict[key[5]].append(my_list[5])
         index = index + 1
 
-import json
+print(my_dict)
+# import json
 
-my_json = json.dumps(my_dict,sort_keys=True, indent=2)
-print(my_json)
+# my_json = json.dumps(my_dict,sort_keys=True, indent=2)
+# print(my_json)
